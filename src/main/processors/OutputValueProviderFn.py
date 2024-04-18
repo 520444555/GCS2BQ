940d497b818b85dc3d1246e05042e07091d8037f
Amit Kudtarkar <58769363+520444555@users.noreply.github.com>
2024-04-18 21:25:29 +0530

    Update OutputValueProviderFn.py

--- src/main/processors/OutputValueProviderFn.py
+++ src/main/processors/OutputValueProviderFn.py
@@ -1,1 +1,15 @@
+import apache_beam as beam
+import logging
 
+class OutputValueProviderFn(beam.DoFn):
+        def __init__(self,filenames):
+                self.filenames=filenames
+
+
+        def process(self,unused_elm):
+
+                logging.info('Runtime Filename is : %s',self.filenames.get())
+                logging.info('Runtime Filename type is : %s',type(self.filenames.get()))
+                files=self.filenames.get().split(',')
+                for file in files:
+                        yield file
